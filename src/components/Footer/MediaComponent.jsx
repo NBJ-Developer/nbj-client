7d31a009558c5d0a32faf5617042c177b93310e4
Salaudeen-Hafeez <amplifiedsalaudeenak21@gmail.com>
2022-12-09 22:05:09 +0100

    removed background color fro media component

--- src/components/Footer/MediaComponent.jsx
+++ src/components/Footer/MediaComponent.jsx
@@ -30,7 +30,6 @@ function MediaComponent() {
           mt: 2,
           py: {xs: 0.6, md: 0},
           px: {xs: 4, md: 0},
-          background: {xs: '#d9d9d9', md: 'none'},
           borderRadius: '5px',
           display: 'flex',
           justifyContent: 'space-between',
@@ -38,18 +37,18 @@ function MediaComponent() {
           <Link href="#">
             <LinkedInIcon sx={{
               mr: 6,
-              color: {xs: '', md: '#ffffff'},
+              color: '#ffffff',
             }}/>
           </Link>
           <Link href="https://twitter.com/nbj_vip" target={'_blank'}>
             <TwitterIcon sx={{
               mr: 6,
-              color: {xs: '', md: '#ffffff'},
+              color:  '#ffffff',
             }}/>
           </Link>
           <Link href="#">
             <InstagramIcon sx={{
-              color: {xs: '', md: '#ffffff'},
+              color: '#ffffff',
             }}/>
           </Link>
         </Box>
